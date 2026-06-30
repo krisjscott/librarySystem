bd50e9b614be9edea1097f3a85de45cedbecabcf
Krisjscott <krishkumar6566@gmail.com>
2026-06-30 18:07:50 +0530

    Added APP file

---
+++ app/src/main/java/org/example/ErrorHandling/RestExceptionHandler.java
@@ -0,0 +1,29 @@
+package org.example.ErrorHandling;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({BookNotFoundException.class})
+    protected ResponseEntity<Object> handleNotFound(
+            Exception ex, WebRequest request){
+        return handleExceptionInternal(ex, "Book NOT found",
+                new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler({BookIdMismatchException.class})
+    public ResponseEntity<Object> handleBadRequest(
+            Exception ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getLocalizedMessage(),
+                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
---
+++ app/src/test/java/org/example/AppTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AppTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void whenGetAllBooks_thenStatus200() throws Exception {
+        mockMvc.perform(get("/api/books"))
+               .andExpect(status().isOk());
+    }
+
+    @Test
+    void whenGetBookByInvalidId_thenStatus404() throws Exception {
+        mockMvc.perform(get("/api/books/999"))
+               .andExpect(status().isNotFound());
+    }
+}
