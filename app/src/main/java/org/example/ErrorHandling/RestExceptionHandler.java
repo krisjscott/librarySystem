53a2f923ed6332389b25d3c23c84b530d0cabcc0
Krisjscott <krishkumar6566@gmail.com>
2026-07-14 16:06:16 +0530

    Added and updated existing and new exceptions

--- app/src/main/java/org/example/ErrorHandling/RestExceptionHandler.java
+++ app/src/main/java/org/example/ErrorHandling/RestExceptionHandler.java
@@ -1,29 +1,76 @@
 package org.example.ErrorHandling;
-
-import org.springframework.http.HttpHeaders;
+import jakarta.servlet.http.HttpServletRequest;
+import org.example.Dto.ExceptionResponseDto;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.time.LocalDateTime;
 
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+@RestControllerAdvice
+public class RestExceptionHandler {
 
-@ControllerAdvice
-public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<ExceptionResponseDto> handleBookNotFoundException(
+            BookNotFoundException ex, HttpServletRequest request){
 
-    @ExceptionHandler({BookNotFoundException.class})
-    protected ResponseEntity<Object> handleNotFound(
-            Exception ex, WebRequest request){
-        return handleExceptionInternal(ex, "Book NOT found",
-                new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exceptionResponseDto);
     }
 
-    @ExceptionHandler({BookIdMismatchException.class})
-    public ResponseEntity<Object> handleBadRequest(
-            Exception ex, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getLocalizedMessage(),
-                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    @ExceptionHandler(BookIdMismatchException.class)
+    public ResponseEntity<ExceptionResponseDto> handleBookIdMismatchException(
+            BookIdMismatchException ex, HttpServletRequest request){
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
+                LocalDateTime.now(),
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI());
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(exceptionResponseDto);
     }
+
+    @ExceptionHandler(DuplicateResouceException.class)
+    public ResponseEntity<ExceptionResponseDto> DuplicareResourceException(
+            DuplicateResouceException ex, HttpServletRequest request){
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
+                LocalDateTime.now(),
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(exceptionResponseDto);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ExceptionResponseDto> handleRuntimeException(RuntimeException ex, HttpServletRequest request){
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exceptionResponseDto);
+    }
+
+
 }
