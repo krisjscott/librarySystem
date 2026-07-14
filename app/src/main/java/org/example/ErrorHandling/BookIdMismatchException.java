cebdf911c81391b571c620500e17c94a96a53d69
Krisjscott <krishkumar6566@gmail.com>
2026-07-14 16:04:12 +0530

    Created and Update BookIdMismatchException

--- app/src/main/java/org/example/ErrorHandling/BookIdMismatchException.java
+++ app/src/main/java/org/example/ErrorHandling/BookIdMismatchException.java
@@ -1,7 +1,5 @@
 package org.example.ErrorHandling;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 public class BookIdMismatchException extends RuntimeException {
     public BookIdMismatchException() {
         super("Mismatched Book ID");
