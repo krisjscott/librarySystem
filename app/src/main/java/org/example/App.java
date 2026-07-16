8d4d4d27d8fb54494bc6c5dc225ac2e517f0984e
Krisjscott <krishkumar6566@gmail.com>
2026-07-17 01:18:57 +0530

    Update App entity scan to point at Entity package

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,14 +3,13 @@
  */
 package org.example;
 
-import org.example.Repository.BookRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("org.example.Repository")
-@EntityScan("org.example.Model")
+@EntityScan("org.example.Entity")
 @SpringBootApplication
 public class App {
 
