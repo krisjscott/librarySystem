48bafc7927a5c362247307dc7650275bbdf37e83
Krisjscott <krishkumar6566@gmail.com>
2026-07-17 01:19:11 +0530

    Add query to check if a book is issued in BookRepository

--- app/src/main/java/org/example/Repository/BookRepository.java
+++ app/src/main/java/org/example/Repository/BookRepository.java
@@ -1,13 +1,18 @@
 package org.example.Repository;
 
-import org.example.Model.Book;
-import org.springframework.data.repository.CrudRepository;;
+import org.example.Entity.Book;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 
     Book findByTitle(String title);
     Book findByAuthor(String author);
+
+    @Query("SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END FROM Book b WHERE b.title = :title AND b.issuedBy = true")
+    boolean ifIssuedOrNot(@Param("title") String title);
 
     boolean existsByTitle(String title);
 }
